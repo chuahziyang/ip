c2f1d4454d9e515621d7456504abfbbe326d1615
chuahziyang <czyang2002@gmail.com>
2025-01-24 15:52:47 +0800

    Level-1

--- src/main/java/Ziyang.java
+++ src/main/java/Ziyang.java
@@ -4,7 +4,18 @@ public class Ziyang {
       System.out.println("Hello! I'm Zi Yang");
       System.out.println("What can I do for you?");
       System.out.println("---------------------------");
+
+      String input = System.console().readLine();
+      while (input.equals("bye") == false) {
+        System.out.println("---------------------------");
+        System.out.println(input);
+        System.out.println("---------------------------");
+        input = System.console().readLine();
+      }
+
+      System.out.println("---------------------------");
       System.out.println("Bye. Hope to see you again soon!");
+      System.out.println("---------------------------");
     }
 }
 
