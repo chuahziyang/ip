c9442dfb6daacec95406b54a91a2dc883fcbabad
chuahziyang <czyang2002@gmail.com>
2025-01-24 15:46:05 +0800

    Level-0

---
+++ src/main/java/Ziyang.java
@@ -0,0 +1,10 @@
+
+public class Ziyang {
+    public static void main(String[] args) {
+      System.out.println("Hello! I'm Zi Yang");
+      System.out.println("What can I do for you?");
+      System.out.println("---------------------------");
+      System.out.println("Bye. Hope to see you again soon!");
+    }
+}
+
