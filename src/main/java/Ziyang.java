96d1a126c45b9ae8561a806b40c8d85f1270811a
chuahziyang <czyang2002@gmail.com>
2025-01-24 16:04:16 +0800

    Level-2

--- src/main/java/Ziyang.java
+++ src/main/java/Ziyang.java
@@ -5,11 +5,25 @@ public class Ziyang {
       System.out.println("What can I do for you?");
       System.out.println("---------------------------");
 
+      String[] items = new String[100];
+
       String input = System.console().readLine();
+      int i = 0;
       while (input.equals("bye") == false) {
-        System.out.println("---------------------------");
-        System.out.println(input);
-        System.out.println("---------------------------");
+        if (input.equals("list")) {
+          System.out.println("---------------------------");
+          for (int j = 0; j < i; j++) {
+            System.out.println(j + ". " + items[j]);
+          }
+          System.out.println("---------------------------");
+        }
+        else{
+          System.out.println("---------------------------");
+          items[i] = input;
+          i++;
+          System.out.println("added: " + input);
+          System.out.println("---------------------------");
+        }
         input = System.console().readLine();
       }
 
