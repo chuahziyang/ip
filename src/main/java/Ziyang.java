99113651758ff4f4b066bb5125e43b0a1f787f48
chuahziyang <czyang2002@gmail.com>
2025-01-31 10:02:20 +0800

    Level-5

--- src/main/java/Ziyang.java
+++ src/main/java/Ziyang.java
@@ -11,50 +11,56 @@ public class Ziyang {
       wrapPrint("Hello! I'm Zi Yang\nWhat can I do for you?");
       Task[] items = new Task[100];
       Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-      System.out.println("Enter username");
 
       String input = myObj.nextLine();
       int i = 0;
       while (true) {
-        if(input.equals("bye") == true){
-          break;
-        }
-        else if (input.equals("list")) {
-          String msg = "Here are the tasks in your list:\n";
-          for (int j = 0; j < i; j++) {
-            msg += j + ". " + items[j].toString() + "\n";
+        try{
+          if(input.equals("bye") == true){
+            break;
           }
-          wrapPrint(msg);
+          else if (input.equals("list")) {
+            String msg = "Here are the tasks in your list:\n";
+            for (int j = 0; j < i; j++) {
+              msg += j + ". " + items[j].toString() + "\n";
+            }
+            wrapPrint(msg);
+          }
+          else if (input.startsWith("mark")) {
+            String j = input.split(" ")[1];
+            items[Integer.parseInt(j)].mark();
+            wrapPrint("Nice! I've marked this task as done:\n " + items[Integer.parseInt(j)].toString());
+          }
+          else if (input.startsWith("todo")) {
+            String j = input.split(" ")[1];
+            items[i] = new Task(j, Task.Type.T, "", "");
+            i++;
+            wrapPrint("added: " + j);
+          }
+          else if (input.startsWith("deadline")) {
+            String j = input.split(" ")[1];
+            String deadline = input.split(" ")[3];;
+            items[i] = new Task(j, Task.Type.D, "", deadline);
+            i++;
+            wrapPrint("added: " + j);
+          }
+          else if (input.startsWith("event")) {
+            String j = input.split(" ")[1];
+            String start = input.split(" ")[3];
+            String end = input.split(" ")[5];
+            items[i] = new Task(j, Task.Type.E, start, end);
+            i++;
+            wrapPrint("added: " + j);
+          }
+          else{
+            wrapPrint("I don't understand what you want to do. Please try again.");
+          }
+          input = myObj.nextLine();
         }
-        else if (input.startsWith("mark")) {
-          String j = input.split(" ")[1];
-          items[Integer.parseInt(j)].mark();
-          wrapPrint("Nice! I've marked this task as done:\n " + items[Integer.parseInt(j)].toString());
+        catch (Exception e){
+          wrapPrint("Incorrect input, please try again.");
+          input = myObj.nextLine();
         }
-        else if (input.startsWith("todo")) {
-          String j = input.split(" ")[1];
-          items[i] = new Task(j, Task.Type.T, "", "");
-          i++;
-          wrapPrint("added: " + j);
-        }
-        else if (input.startsWith("deadline")) {
-          String j = input.split(" ")[1];
-          String deadline = input.split(" ")[3];;
-          items[i] = new Task(j, Task.Type.D, "", deadline);
-          i++;
-          wrapPrint("added: " + j);
-        }
-        else if (input.startsWith("event")) {
-          String j = input.split(" ")[1];
-          String start = input.split(" ")[3];
-          String end = input.split(" ")[5];
-          items[i] = new Task(j, Task.Type.E, start, end);
-          i++;
-          wrapPrint("added: " + j);
-        }
-
-
-        input = myObj.nextLine();
       }
 
       System.out.println("---------------------------");
